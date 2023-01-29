a668ec23ec3fdc8cb35961ee18252c23c36b1a5d
yinkakun <yinkakun@gmail.com>
2023-01-29 16:07:26 +0100

    a method with some other parameter

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,29 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        self.area() > other_rectangle.area()
+    }
 }
 
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 20,
         height: 50,
     };
 
-    println!("The rectangle: {:?}", rect);
-    println!("The area of this rectangle is {}", rect.area());
+    let rect2 = Rectangle {
+        width: 10,
+        height: 45,
+    };
 
-    if rect.width() {
-        println!("This rectangle has a non zero width: {}", rect.width);
+    println!("The rectangle: {:?}", rect1);
+    println!("The area of this rectangle is {}", rect1.area());
+
+    if rect1.width() {
+        println!("This rectangle has a non zero width: {}", rect1.width);
     }
+
+    println!("Can rect1 hold rect2?: {}", rect1.can_hold(&rect2));
 }
