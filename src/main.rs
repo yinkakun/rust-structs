d0087fbe5aae4e983997473808e5a4e0e44db060
yinkakun <yinkakun@gmail.com>
2023-01-29 15:50:55 +0100

    write struct and method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        width: 20,
+        height: 50,
+    };
+
+    println!("The rectangle: {:?}", rect);
+    println!("The area of this rectangle is {}", rect.area());
+
+    if rect.width() {
+        println!("This rectangle has a non zero width: {}", rect.width);
+    }
 }
